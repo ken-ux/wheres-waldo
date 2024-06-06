7f704a9c62d4366a042f0082d99d7664df8ee7d9
Kenny Nguyen <sokenns@gmail.com>
2024-06-06 15:09:47 -0700

    Add leaderboard link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,11 +1,18 @@
 export default function Nav() {
   return (
     <nav className="bg-blue-200 p-4">
-      <a href="/">
-        <p className="test-border m-auto max-w-screen-xl text-xl font-bold">
-          Where's Waldo?
-        </p>
-      </a>
+      <ul className="test-border m-auto flex max-w-screen-xl justify-between">
+        <li>
+          <a href="/" className="text-xl font-bold">
+            Where's Waldo?
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <p>Leaderboards</p>
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
