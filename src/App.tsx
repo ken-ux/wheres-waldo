c38190c9b1447b9f4b8b3b68d2b241e48a2321a1
Kenny Nguyen <sokenns@gmail.com>
2024-06-06 13:22:10 -0700

    Map out site layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 function App() {
   return (
     <>
-      <p className="text-red-500">Hello, world.</p>
+      <nav className="bg-blue-200 py-4">
+        <h1 className="test-border m-auto max-w-screen-xl text-lg">
+          Where's Waldo?
+        </h1>
+      </nav>
+      <main className="bg-red-200 py-4">
+        <div className="test-border m-auto max-w-screen-xl">Main</div>
+      </main>
+      <footer className="bg-yellow-200 py-4">
+        <div className="test-border m-auto max-w-screen-xl">Footer</div>
+      </footer>
     </>
   );
 }
