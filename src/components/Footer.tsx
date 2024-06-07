4e495f3c61ead95f247c45acc3b22bb2ee7e8b34
Kenny Nguyen <sokenns@gmail.com>
2024-06-07 14:20:42 -0700

    Add GitHub logo to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
+import GitHubIcon from "../assets/github-mark.svg";
+
 export default function Footer() {
   return (
     <footer className="bg-yellow-200 p-4">
-      <div className="test-border m-auto max-w-screen-xl">GitHub</div>
+      <div className="test-border m-auto flex max-w-screen-xl justify-center">
+        <a href="https://github.com/ken-ux/wheres-waldo">
+          <img src={GitHubIcon} alt="GitHub Repository" className="h-8 w-8" />
+        </a>
+      </div>
     </footer>
   );
 }
