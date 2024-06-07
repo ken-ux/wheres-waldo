98509ac4621d56bcf72ee26272c7c32a8bcf4fd4
Kenny Nguyen <sokenns@gmail.com>
2024-06-07 14:46:02 -0700

    Use Outlet in main element for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Nav />
       <main className="flex flex-1 justify-center bg-red-200 px-4 py-12">
-        <Home />
+        <Outlet />
       </main>
       <Footer />
     </>
