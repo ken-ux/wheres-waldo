cd46fa3c11748554a7bbd79e63f75e9f95804219
Kenny Nguyen <sokenns@gmail.com>
2024-06-07 14:47:23 -0700

    Align links in nav bar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Nav() {
   return (
     <nav className="bg-blue-200 p-4">
-      <ul className="test-border m-auto flex max-w-screen-xl justify-between">
+      <ul className="test-border m-auto flex max-w-screen-xl justify-between items-center">
         <li>
           <Link to="/" className="text-xl font-bold">
             Where's Waldo?
