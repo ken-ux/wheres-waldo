fb1f639b876af1730c874b5ee93e07f338ef13a2
Kenny Nguyen <sokenns@gmail.com>
2024-06-07 22:04:52 -0700

    Open GitHub repo in new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,12 @@ export default function Footer() {
   return (
     <footer className="bg-yellow-200 p-4">
       <div className="test-border m-auto flex max-w-screen-xl justify-center">
-        <a href="https://github.com/ken-ux/wheres-waldo">
+        <a
+          href="https://github.com/ken-ux/wheres-waldo"
+          target="_blank"
+          rel="noopener"
+          aria-label="Github"
+        >
           <img src={GitHubIcon} alt="GitHub Repository" className="h-8 w-8" />
         </a>
       </div>
