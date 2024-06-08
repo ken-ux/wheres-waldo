9d117559721894b46cbced19070539afd37a8dda
Kenny Nguyen <sokenns@gmail.com>
2024-06-07 22:00:48 -0700

    Add goal cards to instructions page

---
+++ src/components/GoalCard.tsx
@@ -0,0 +1,8 @@
+export default function GoalCard() {
+  return (
+    <div className="test-border">
+      <div>img goes here</div>
+      <p>Name</p>
+    </div>
+  );
+}
--- src/pages/Instructions.tsx
+++ src/pages/Instructions.tsx
@@ -1,3 +1,16 @@
+import GoalCard from "../components/GoalCard";
+
 export default function Instructions() {
-  return <div>Hello</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-8">
+      <h1 className="text-2xl font-semibold">
+        You need to find these three characters or objects:
+      </h1>
+      <div className="flex gap-2">
+        <GoalCard />
+        <GoalCard />
+        <GoalCard />
+      </div>
+    </div>
+  );
 }
