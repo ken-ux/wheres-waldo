e24c4d9acd59af079af00fb4811f8793d1748948
Kenny Nguyen <sokenns@gmail.com>
2024-06-07 22:03:20 -0700

    Enlarge text

--- src/pages/Instructions.tsx
+++ src/pages/Instructions.tsx
@@ -3,7 +3,7 @@ import GoalCard from "../components/GoalCard";
 export default function Instructions() {
   return (
     <div className="flex flex-col items-center justify-center gap-8">
-      <h1 className="text-2xl font-semibold">
+      <h1 className="text-3xl font-semibold">
         You need to find these three characters or objects:
       </h1>
       <div className="flex gap-2">
