0d06031c9f4e2e65f773a605b0d308d339fd9729
Kenny Nguyen <sokenns@gmail.com>
2024-06-07 22:00:33 -0700

    Use flex gap instead of margin to separate flex children

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import GameCard from "../components/GameCard";
 
 export default function Home() {
   return (
-    <div className="test-border m-auto flex w-full max-w-screen-xl flex-col items-center">
-      <h1 className="mb-8 text-3xl font-semibold">Choose a Game</h1>
+    <div className="test-border m-auto flex w-full max-w-screen-xl flex-col items-center gap-8">
+      <h1 className="text-3xl font-semibold">Choose a Game</h1>
       <div className="flex w-full flex-col flex-wrap justify-center gap-4 md:flex-row">
         <GameCard />
         <GameCard />
