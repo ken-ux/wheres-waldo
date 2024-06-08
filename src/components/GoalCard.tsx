ccafc70cba46e1c94dd3129beea7fb187a71aea9
Kenny Nguyen <sokenns@gmail.com>
2024-06-07 22:03:09 -0700

    Add start button

--- src/components/GoalCard.tsx
+++ src/components/GoalCard.tsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function GoalCard() {
   return (
     <div className="test-border">
       <div>img goes here</div>
       <p>Name</p>
+      <Link to="/">Start</Link>
     </div>
   );
 }
