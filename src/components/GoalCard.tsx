ee55f6ee393ed52a74673eaac6374caf47b675de
Kenny Nguyen <sokenns@gmail.com>
2024-06-10 13:59:11 -0700

    Style cards based on location path

--- src/components/GoalCard.tsx
+++ src/components/GoalCard.tsx
@@ -1,3 +1,6 @@
+// import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
 export default function GoalCard({
   image,
   title,
@@ -5,9 +8,26 @@ export default function GoalCard({
   image: string | undefined;
   title: string | undefined;
 }) {
+  const location = useLocation();
+  let game_screen = false;
+
+  if (location.pathname.includes("game")) {
+    game_screen = true;
+  }
+
   return (
-    <div className="test-border flex flex-1 flex-col gap-4 text-center">
-      <img src={image} alt={title} className="aspect-square w-full" />
+    <div
+      className={
+        game_screen
+          ? "test-border flex flex-1 items-center gap-2"
+          : "test-border flex flex-1 flex-col gap-4 text-center"
+      }
+    >
+      <img
+        src={image}
+        alt={title}
+        className={game_screen ? "h-24 w-24" : "aspect-square w-full"}
+      />
       <p>{title}</p>
     </div>
   );
