34a7b9ec2258b0dbe1ff96e1e6b4444eb0ba6d6c
Kenny Nguyen <sokenns@gmail.com>
2024-06-14 02:01:27 -0700

    Add state and effect for calculating elapsed time

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
 export default function Timer() {
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
-      <h1 className="text-2xl">Timer: 00:00:00</h1>
+      <h1 className="text-2xl">Timer: 00:00:00 {count}</h1>
     </div>
   );
 }
