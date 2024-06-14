a7f3372a51d144a28a0b8d4b1b87288f9a98c981
Kenny Nguyen <sokenns@gmail.com>
2024-06-14 16:28:04 -0700

    Add form for sending leaderboard data

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,9 +1,39 @@
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 export default function GameOver({ time }: { time: number }) {
+  const { difficulty } = useParams();
+  const formHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
   return (
     <div className="absolute flex h-full w-full flex-col items-center justify-center bg-slate-800/90 text-white">
-      <p>Congratulations! You finished this game in {time} seconds.</p>
-      <Link to="/" className="rounded bg-teal-600 px-3 py-1">
+      <p className="mb-6 text-2xl">
+        Congratulations! You finished this game in {time} seconds.
+      </p>
+      <form
+        onSubmit={(e) => formHandler(e)}
+        className="mb-6 flex flex-col items-center"
+      >
+        <label htmlFor="name" className="mb-2">
+          Add your name to the leaderboards:
+        </label>
+        <input type="hidden" name="score" value={time} />
+        <input type="hidden" name="difficulty" value={difficulty} />
+        <div className="flex items-center gap-2">
+          <input
+            type="text"
+            name="name"
+            required
+            className="rounded px-2 py-1 text-black"
+          />
+          <button type="submit" className="rounded bg-teal-600 px-3 py-1">
+            Submit
+          </button>
+        </div>
+      </form>
+      <Link to="/" className="rounded bg-red-600 px-3 py-1">
         Restart
       </Link>
     </div>
