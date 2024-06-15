272786bec3f29f3f1c4894d6d7a75335fdcee81a
Kenny Nguyen <sokenns@gmail.com>
2024-06-15 15:27:27 -0700

    Style home page

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -22,19 +22,21 @@ export default function GameCard({
   }
 
   return (
-    <div className="test-border flex w-full flex-1 flex-col items-center gap-2">
+    <div className="flex w-full flex-1 flex-col items-center rounded-lg border border-teal-600/50 shadow-lg transition duration-300 hover:scale-[1.01] hover:shadow-xl">
       <img
         src={img_src}
         alt={`${difficulty} game mode`}
-        className="aspect-video object-cover"
+        className="aspect-video rounded-t-lg object-cover"
       />
-      <p className="text-lg">Game Title</p>
-      <Link
-        to={`/instructions/${difficulty}`}
-        className="rounded bg-teal-600 px-3 py-1 text-white"
-      >
-        Select
-      </Link>
+      <div className="flex w-full flex-col items-center gap-2 rounded-b-lg border-t border-teal-600/50 bg-white p-4">
+        <p className="text-lg font-semibold capitalize">{difficulty}</p>
+        <Link
+          to={`/instructions/${difficulty}`}
+          className="rounded bg-teal-600 px-3 py-1 text-white"
+        >
+          Select
+        </Link>
+      </div>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,10 @@ import GameCard from "../components/GameCard";
 
 export default function Home() {
   return (
-    <div className="test-border m-auto flex w-full max-w-screen-xl flex-col items-center gap-8">
-      <h1 className="text-2xl font-semibold md:text-3xl">Choose a Game</h1>
+    <div className="m-auto flex w-full max-w-screen-xl flex-col items-center gap-8">
+      <h1 className="text-2xl font-semibold md:text-3xl">
+        Select a <span className="text-teal-600">Game Mode</span>
+      </h1>
       <div className="flex w-full flex-col flex-wrap justify-center gap-4 md:flex-row">
         <GameCard difficulty="easy" />
         <GameCard difficulty="medium" />
