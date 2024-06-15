9c829a5b9b7c21c8e9f0a49881250b4239c1a3ef
Kenny Nguyen <sokenns@gmail.com>
2024-06-14 19:35:32 -0700

    Add placeholder table to leaderboard

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,3 +1,67 @@
 export default function Leaderboard() {
-  return <div>Leaderboard</div>;
+  return (
+    <div className="test-border w-full max-w-screen-xl text-center">
+      <h1 className="text-3xl">Leaderboard</h1>
+      <div>
+        <h2>Easy</h2>
+        <table className="test-border m-auto">
+          <tr className="*:test-border">
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Time (seconds)</th>
+          </tr>
+          <tr className="*:test-border">
+            <td>1</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="*:test-border">
+            <td>2</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <h2>Medium</h2>
+        <table className="test-border m-auto">
+          <tr className="*:test-border">
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Time (seconds)</th>
+          </tr>
+          <tr className="*:test-border">
+            <td>1</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="*:test-border">
+            <td>2</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <h2>Hard</h2>
+        <table className="test-border m-auto">
+          <tr className="*:test-border">
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Time (seconds)</th>
+          </tr>
+          <tr className="*:test-border">
+            <td>1</td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr className="*:test-border">
+            <td>2</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
