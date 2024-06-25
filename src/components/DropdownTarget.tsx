cf67517f416eb81f207f8a8fd2870feff59d7c1a
Kenny Nguyen <sokenns@gmail.com>
2024-06-25 01:40:41 -0700

    Define boundary for successful guesses

--- src/components/DropdownTarget.tsx
+++ src/components/DropdownTarget.tsx
@@ -1,3 +1,5 @@
+import { useParams } from "react-router-dom";
+
 export default function DropdownTarget({
   id,
   image,
@@ -21,37 +23,48 @@ export default function DropdownTarget({
   setGameOver: React.Dispatch<React.SetStateAction<boolean>> | undefined;
   position: { x: number; y: number } | undefined;
 }) {
+  const { difficulty } = useParams();
   let disabled = false;
 
-  const clickHandler = () => {
-    console.log(title);
+  const clickHandler = async () => {
+    const boundary = 50;
+    const response = await fetch(
+      `${import.meta.env.VITE_SERVER}/goal?difficulty=${difficulty}&desc=${title}`,
+    );
+    const data = await response.json();
+    console.log(data);
     console.log(position?.x, position?.y);
 
     if (setTooltipOpen) {
       setTooltipOpen(false);
     }
-    if (
-      goalsCompleted !== undefined &&
-      setGoalsCompleted !== undefined &&
-      setGameOver !== undefined
-    ) {
-      const newGoalsCompleted = goalsCompleted;
-      newGoalsCompleted[id] = true;
-      setGoalsCompleted(newGoalsCompleted);
+
+    if (goalsCompleted && setGoalsCompleted && setGameOver && position) {
+      // Check if click was close enough to the answer.
+      if (
+        position.x < data.pos_x + boundary &&
+        position.x > data.pos_x - boundary &&
+        position.y < data.pos_y + boundary &&
+        position.y > data.pos_y - boundary
+      ) {
+        const newGoalsCompleted = goalsCompleted;
+        newGoalsCompleted[id] = true;
+        setGoalsCompleted(newGoalsCompleted);
+      }
       if (goalsCompleted[1] && goalsCompleted[2] && goalsCompleted[3]) {
         setGameOver(true);
       }
     }
   };
 
-  if (goalsCompleted !== undefined && goalsCompleted[id] === true) {
+  if (goalsCompleted && goalsCompleted[id] === true) {
     disabled = true;
   }
 
   return (
     !disabled && (
       <div
-        className={"flex items-center bg-white hover:bg-slate-500"}
+        className={"hover:bg-slate-boundary0 flex items-center bg-white"}
         onClick={clickHandler}
       >
         <img
