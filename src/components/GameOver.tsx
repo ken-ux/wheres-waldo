b185c5ce7cdc10120490f0a68a7825b32349c190
Kenny Nguyen <sokenns@gmail.com>
2024-06-26 19:57:52 -0700

    Add max length to input and disable button when processing request

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+
 export default function GameOver({ time }: { time: number }) {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
   const { difficulty } = useParams();
 
   const formHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsDisabled(true);
 
     // Extend typing to prevent errors when accessing values.
     const elements = e.currentTarget.elements as HTMLFormControlsCollection & {
@@ -30,9 +35,11 @@ export default function GameOver({ time }: { time: number }) {
         body: JSON.stringify(formData),
       });
 
-      const result = await response.json();
-      console.log("Response:", result);
+      if (response.status === 200) {
+        setIsSubmitted(true);
+      }
     } catch (error) {
+      setIsDisabled(false);
       console.error("Error:", error);
     }
   };
@@ -43,29 +50,38 @@ export default function GameOver({ time }: { time: number }) {
         Congratulations! You found the characters in {Math.floor(time / 60)}{" "}
         minute(s) {time % 60} second(s).
       </p>
-      <form
-        onSubmit={(e) => formHandler(e)}
-        className="mb-6 flex flex-col items-center"
-      >
-        <label htmlFor="name" className="mb-2 text-lg">
-          Add your name to the leaderboards:
-        </label>
-        <input type="hidden" name="score" value={time} />
-        <input type="hidden" name="difficulty" value={difficulty} />
-        <div className="flex items-center gap-2">
-          <input
-            id="name"
-            type="text"
-            name="name"
-            required
-            className="rounded px-2 py-1 text-black"
-            autoComplete="given-name"
-          />
-          <button type="submit" className="rounded bg-teal-600 px-3 py-1">
-            Submit
-          </button>
-        </div>
-      </form>
+      {!isSubmitted ? (
+        <form
+          onSubmit={(e) => formHandler(e)}
+          className="mb-6 flex flex-col items-center"
+        >
+          <label htmlFor="name" className="mb-2 text-lg">
+            Add your name to the leaderboards:
+          </label>
+          <input type="hidden" name="score" value={time} />
+          <input type="hidden" name="difficulty" value={difficulty} />
+          <div className="flex items-center gap-2">
+            <input
+              id="name"
+              type="text"
+              name="name"
+              required
+              className="rounded px-2 py-1 text-black"
+              autoComplete="given-name"
+              maxLength={20}
+            />
+            <button
+              type="submit"
+              className="rounded bg-teal-600 px-3 py-1 disabled:bg-gray-500"
+              disabled={isDisabled}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      ) : (
+        <p className="mb-6 text-xl">Submitted!</p>
+      )}
       <Link to="/" className="rounded bg-red-600 px-3 py-1">
         Restart
       </Link>
